6835258001fa00558bad2d8c1f9b43b6ba4674cc
rezanikafarin <rezanikafarin100@gmail.com>
2024-11-13 21:56:50 +0330

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import store from "./store/store";
-import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { router } from "./routes/router";
 import { createRoot } from "react-dom/client";
@@ -13,9 +12,7 @@ import "./index.css";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    {/* <StrictMode> */}
       <RouterProvider router={router} />
-    {/* </StrictMode> */}
   </Provider>
 );
 
