f481c87e8930c9eade4908ec7b4bc2a22dcf05f1
rezanikafarin <rezanikafarin100@gmail.com>
2024-11-23 19:02:01 +0330

    a test in modified:   src/Global/BaseUrl.js

--- src/Global/BaseUrl.js
+++ src/Global/BaseUrl.js
@@ -11,6 +11,7 @@ export const config = {
 
   export const headers = {
     "Content-Type": "application/json",
+    
     Accept: "application/json",
     Authorization:
     "Bearer hL3mLquFhdkhpj6qEfIBfjyOioIMLe34lr6kmQ9S4R5G77zR0sEzQpfL1zC6ZQaveBRK21K1amv4lBz5x3Gu5wySwvuY15ZqRCvV",
