a6909ba6e41b23d33d752f324b3fcc1b1c37b4ca
rezanikafarin <rezanikafarin100@gmail.com>
2024-12-21 21:31:02 +0330

    create citySlice.js

---
+++ src/slices/citySlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BaseURL } from "../Global/BaseUrl";
+
+export const fetchProducts = createAsyncThunk(
+  "citySlice/fetchAPI",
+  async (text) => {
+    try {
+      const res = await axios.get("http://localhost:9000/products");
+        return res.data.filter(d => d.title.indexOf(text) > -1);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+);
+
+const citySlice = createSlice({
+  name: "products",
+  initialState: {
+    data: [],
+    loading: false,
+    errorMessage: "",
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchProducts.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.errorMessage = "error in fetch data";
+        state.loading = false;
+      });
+
+  },
+});
+export default citySlice.reducer;
