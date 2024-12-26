11c255958fee744fb8f200c68fed2564cbea59af
rezanikafarin <rezanikafarin100@gmail.com>
2024-12-26 06:50:32 +0330

    edit for search header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -26,12 +26,14 @@ const Header = () => {
   };
 
   const openSelectCity = () => {
+    document.getElementById("headerinput").style.zIndex = "-1";
     document.querySelector(".header__select-city").style.display = "flex";
     document.querySelector("html").style.overflowY = "hidden";
     document.querySelector("html").style.scrollbarGutter = "stable";
   };
 
   const closeSelectCity = () => {
+    document.getElementById("headerinput").style.zIndex = "1";
     document.querySelector(".header__select-city").style.display = "none";
     document.querySelector("html").style.overflowY = "scroll";
   };
@@ -39,92 +41,92 @@ const Header = () => {
     e.preventDefault();
     e.stopPropagation();
   };
+
   return (
     <header className="header">
-        <div className="header__layer">
-          <div className="header__select-city" onClick={closeSelectCity}>
-            <div className="header__box" onClick={(e) => f(e)}>
-              <SelectCity />
-            </div>
+      <div className="header__layer">
+        <div className="header__select-city" onClick={closeSelectCity}>
+          <div className="header__box" onClick={(e) => f(e)}>
+            <SelectCity />
           </div>
         </div>
-        <nav className="header__section">
-          <span to="/register" className="header__logo"></span>
-          <span className="header__balance" onClick={openSelectCity}>
-            <span>اراک</span>
-            <span style={{ marginLeft: ".25rem" }}></span>
-            <Location width="1rem" height="1rem" color={"rgba(0, 0, 0, .56)"} />
-          </span>
-          {/* <div
+      </div>
+      <nav className="header__section">
+        <span to="/register" className="header__logo"></span>
+        <span className="header__balance" onClick={openSelectCity}>
+          <span>اراک</span>
+          <span style={{ marginLeft: ".25rem" }}></span>
+          <Location width="1rem" height="1rem" color={"rgba(0, 0, 0, .56)"} />
+        </span>
+        {/* <div
           className={
             "header__balance" + (numberLink === 0 ? "header__select" : "")
           }
           onClick={() => selector(0)}
         ></div> */}
-          <NavLink to="/" className="header__balance">
-            <span>صفحه اصلی</span>
-            <span style={{ marginLeft: ".25rem" }}></span>
-            <Home width="1rem" height="1rem" color={"rgba(0, 0, 0, .56)"} />
-          </NavLink>
-          <span style={{ marginRight: "1rem" }}></span>
-          <span className="header__search">
-            <input
-              value={text}
-              className="header__search"
-              placeholder="جستجو"
-              onChange={(e) => dispatch(updateSearch(e.target.value))}
+        <NavLink to="/" className="header__balance">
+          <span>صفحه اصلی</span>
+          <span style={{ marginLeft: ".25rem" }}></span>
+          <Home width="1rem" height="1rem" color={"rgba(0, 0, 0, .56)"} />
+        </NavLink>
+        <span style={{ marginRight: "1rem" }}></span>
+        <span id="headerinput" className="header__search">
+          <input
+            value={text}
+            placeholder="جستجو"
+            onChange={(e) => dispatch(updateSearch(e.target.value))}
+          />
+          <span className="header__search-icon">
+            <SearchIcon
+              width=".85rem"
+              height=".85rem"
+              color={"rgba(0, 0, 0, .4)"}
             />
-            <span className="header__search-icon">
-              <SearchIcon
-                width=".85rem"
-                height=".85rem"
-                color={"rgba(0, 0, 0, .4)"}
-              />
-            </span>
           </span>
-        </nav>
-        <nav className="header__section">
-          {/* <span
+        </span>
+      </nav>
+      <nav className="header__section">
+        {/* <span
           onClick={() => selector(1)}
           className={numberLink === 1 ? "header__select" : ""}
         >
         </span> */}
 
-          {/* <span
+        {/* <span
           onClick={() => selector(2)}
           className={
             "header__balance" + (numberLink === 2 ? "header__select" : "")
           }
         >
         </span> */}
-          <NavLink to="/general" className="header__balance">
-            <span>تولیدکنندگان</span>
-            <span style={{ marginLeft: ".25rem" }}></span>
-            <UsersIcon width="1rem" height="1rem" color="rgba(0,0,0,.56)" />
-          </NavLink>
-          <NavLink className="header__balance">
-            <span>تولید من</span>
-            <span style={{ marginLeft: ".25rem" }}></span>
-            <UserIcon width="1rem" height="1rem" color="rgba(0,0,0,.56)" />
-          </NavLink>
-          <NavLink className="header__balance">
-            <span>پشتیبانی</span>
-            <span style={{ marginLeft: ".25rem" }}></span>
-            <Support width="1rem" height="1rem" color="rgba(0,0,0,.56)" />
-          </NavLink>
+        <NavLink to="/general" className="header__balance">
+          <span>تولیدکنندگان</span>
+          <span style={{ marginLeft: ".25rem" }}></span>
+          <UsersIcon width="1rem" height="1rem" color="rgba(0,0,0,.56)" />
+        </NavLink>
+        <NavLink className="header__balance">
+          <span>تولید من</span>
+          <span style={{ marginLeft: ".25rem" }}></span>
+          <UserIcon width="1rem" height="1rem" color="rgba(0,0,0,.56)" />
+        </NavLink>
+        <NavLink className="header__balance">
+          <span>پشتیبانی</span>
+          <span style={{ marginLeft: ".25rem" }}></span>
+          <Support width="1rem" height="1rem" color="rgba(0,0,0,.56)" />
+        </NavLink>
 
-          <NavLink to="/" className="header__balance">
-            <span>فارسی</span>
-            <span style={{ marginLeft: ".25rem" }}></span>
-            <Language width="1rem" height="1rem" color={"rgba(0, 0, 0, .56)"} />
-          </NavLink>
+        <NavLink to="/" className="header__balance">
+          <span>فارسی</span>
+          <span style={{ marginLeft: ".25rem" }}></span>
+          <Language width="1rem" height="1rem" color={"rgba(0, 0, 0, .56)"} />
+        </NavLink>
 
-          <span onClick={() => selector(3)}>
-            <Link to="/insert-product" className="mybtn mybtn__active">
-              ثبت محصول
-            </Link>
-          </span>
-        </nav>
+        <span onClick={() => selector(3)}>
+          <Link to="/insert-product" className="mybtn mybtn__active">
+            ثبت محصول
+          </Link>
+        </span>
+      </nav>
     </header>
   );
 };
