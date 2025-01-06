e7335bbc0fa68971dee82154aaaaf56245169328
rezanikafarin1937 <rezanikafarin5@gmail.com>
2025-01-06 06:00:09 -0800

    2 commit after install Git for test

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { Star, NoImage, NoPicture } from "../../components";
 import "./product-card.scss";
-import axios from "axios";
 
 const ProductCard = ({ data,cityName,provinceName }) => {
   return (
