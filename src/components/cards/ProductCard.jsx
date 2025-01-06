21a38d87909528300f846d067e3552acb2422c36
rezanikafarin1937 <rezanikafarin700@gmail.com>
2025-01-06 07:07:43 -0800

    6 after install Git

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -5,6 +5,7 @@ import "./product-card.scss";
 /* REza Nikafarin For Test Remote Gitup  2**/
 /* REza Nikafarin For Test Remote Gitup  3**/
 /* REza Nikafarin For Test Remote Gitup  4**/
+/* REza Nikafarin For Test Remote Gitup  5**/
 const ProductCard = ({ data,cityName,provinceName }) => {
   return (
     <Link className="card" to={`/products/show/${data.id}`}>
